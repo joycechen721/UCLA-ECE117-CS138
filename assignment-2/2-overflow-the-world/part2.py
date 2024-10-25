a4e26fd6dfc7fe7eb4df17d07d3cbf9bf795d65f
Fr3ya <38233835+Fr3ya@users.noreply.github.com>
2024-10-25 18:28:59 +0000

    update

--- assignment-2/2-overflow-the-world/part2.py
+++ assignment-2/2-overflow-the-world/part2.py
@@ -7,7 +7,8 @@ r = process([exe.path])
 # gdb.attach(r)
 
 win = exe.symbols["print_flag"]
-payload = #write your payload here, prompt: it should be overwrite the saved base pointer (rbp), positioning the payload right at the saved return address, then add p64(win).
+#write your payload here, prompt: it should be overwrite the saved base pointer (rbp), positioning the payload right at the saved return address, then add p64(win).
+# payload = 
 
 r.recvuntil(b"What's your name? ")
 r.sendline(payload)
